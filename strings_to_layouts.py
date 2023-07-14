8d32121cdf1bad742e1f874e2acfa6526417ec8f
ATKuom <karagoztarik@gmail.com>
2023-07-14 18:47:29 +0100

    14/07 Julian input for strings to layouts part

--- strings_to_layouts.py
+++ strings_to_layouts.py
@@ -17,11 +17,31 @@ from designs import expert_designs
 # It is implemented like checking to see if the input values are known for the functions, if they are, calculate the function and get the output values
 # The other version is finding a way to order the functions in a way that the input values are known for the functions, if they are, calculate the function and get the output values
 
+# UNIT_FROM_CHAR = {'T': Turbine}
 
-def word_to_units(sequence):
-    pass
+# def word_to_units(sequence):
+#     return [UNIT_FROM_CHAR[char]() for char in sequence]
 
 
-if __name__ == "__main__":
-    datalist = expert_designs
-    word_to_units(datalist[1])
+# if __name__ == "__main__":
+#     datalist = expert_designs
+#     word_to_units(datalist[1])
+
+# [T,A,C,H,T,T,C]
+# class for units?
+# while look without backup or some kind of alternative ending to stop
+# #####Profiling if it is slow? Finding the real cause of the speed problems not just assumptions, programs or timing each part to see the performance
+# data oriented programming
+
+# class Turbine:
+#     DecisionVariables = namedtuple('DecisionVariables', 'outlet_pressure')
+#     var: DecisionVariables
+
+#     def __init__(self, *var):
+#         if len(var) == 0:
+#             self.var = #assign randomly
+#         self.var = DecisionVariables(var)
+
+#     def outlet_parameters(self, *inlet_parameters):
+#         # do computation based on var
+#         return outlet_parameters
