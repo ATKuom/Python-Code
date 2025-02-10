04bc3dc2ff5db23000ed7e394389ad489d10132f
ATKuom <karagoztarik@gmail.com>
2025-02-10 23:47:31 +0000

    10/02 mcts laptop update

--- mcts.py
+++ mcts.py
@@ -5,10 +5,42 @@ import torch.nn.functional as F
 import random
 from ZW_utils import std_classes
 from ZW_model import GPT
+from ZW_Opt import *
+from split_functions import bound_creation, layout_to_string_single_1d
+from thermo_validity import *
 
 model = GPT(12, 32, 4, 2, 22, 0.1)
 model.load_state_dict(torch.load("GPT_NA_psitest/M1_model_10.pt"))
 classes = std_classes
+
+
+def evaluation(layout):
+    # 1. One hot encoding from integer
+    layout = layout.astype(int)
+    stringlist = [
+        layout_to_string_single_1d(layout),
+    ]
+    valid_string = validity(stringlist)
+    if len(valid_string) == 0:
+        return -1e6
+    ohe = np.zeros((len(layout), len(classes)))
+    for i, l in enumerate(layout):
+        ohe[i, l] = 1
+    equipment, bounds, x, splitter = bound_creation(ohe)
+    swarmsize_factor = 7
+    nv = len(bounds)
+    particle_size = swarmsize_factor * nv
+    if 5 in equipment:
+        particle_size += -1 * swarmsize_factor
+    if 9 in equipment:
+        particle_size += -2 * swarmsize_factor
+    iterations = 30
+    try:
+        a = PSO(objective_function, bounds, particle_size, iterations, nv, equipment)
+        value = -1 * a.result
+    except:
+        value = -1e6
+    return value
 
 
 class Flowsheet:
@@ -35,17 +67,14 @@ class Flowsheet:
     def check_win(self, state, action):
         if action == None:
             return False
-        column = np.where(state == -1)[0][0]
-        state[column] = action
         if action == 11:
             return True
         return False
 
     def get_value_and_terminated(self, state, action):
         if self.check_win(state, action):
-            # check win is basically checking if it is completed the flowsheet
-            # if it is completed, then we can put in optimizer to get the real value later
-            return 1, True
+            value = evaluation(self.get_encoded_state(state))
+            return value, True
         if np.sum(self.get_valid_moves(state)) == 0:
             return 0, True
         return 0, False
@@ -54,10 +83,10 @@ class Flowsheet:
         return -player
 
     def get_opponent_value(self, value):
-        return -value
+        return value
 
     def change_perspective(self, state, player):
-        return state * player
+        return state
 
     def get_encoded_state(self, state):
         column = np.where(state == -1)[0][0]
@@ -99,7 +128,7 @@ class Node:
             q_value = 0
         else:
             # 1 - because of switching player the child position is the opponent position
-            q_value = 1 - ((child.value_sum / child.visit_count) + 1) / 2
+            q_value = ((child.value_sum / child.visit_count) + 1) / 2
         return (
             q_value
             + self.args["C"]
@@ -142,8 +171,9 @@ class MCTS:
         )[:, -1, :]
         policy = torch.softmax(policy, axis=-1).squeeze(0).detach().numpy()
 
-        # policy = (1-self.args["dirichlet_epsilon"])*policy + self.args["dirichlet_epsilon"]\
-        #     *np.random.dirichlet([self.args["dirichlet_alpha"]]*self.game.action_size)
+        # policy = (1 - self.args["dirichlet_epsilon"]) * policy + self.args[
+        #     "dirichlet_epsilon"
+        # ] * np.random.dirichlet([self.args["dirichlet_alpha"]] * self.game.action_size)
 
         # all moves are valid if we are not masking valid_moves = self.game.get_valid_moves(state)
         # policy*=valid_moves
@@ -168,7 +198,6 @@ class MCTS:
                         self.game.get_encoded_state(node.state), dtype=torch.long
                     ).unsqueeze(0)
                 )[:, -1, :]
-                print(node.state)
                 policy = torch.softmax(policy, axis=-1).squeeze(0).detach().numpy()
                 # valid_moves = self.game.get_valid_moves(node.state)
                 # policy = policy * valid_moves
@@ -193,44 +222,34 @@ ttt = Flowsheet()
 player = 1
 args = {
     "C": 1.41,
-    "num_searches": 1000,
+    "num_searches": 10,
 }
 mcts = MCTS(ttt, args, model)
 state = ttt.get_initial_state()
-# policy = mcts.model(
-#             torch.tensor(mcts.game.get_encoded_state(state),dtype=torch.long).unsqueeze(0))
-# print(policy)
-# policy = F.softmax(policy,dim=-1).squeeze(0).detach().numpy()
-# print(policy)
-# valid_moves = ttt.get_valid_moves(state)
-# print(valid_moves)
+# state = [0, 1, 5, 2, 3, 9, 4, 7, 5, 4, 7, 4, 11, -1]
+# state = np.array(state)
+# sf = ttt.get_encoded_state(state)
+# print(sf)
+# val = evaluation(sf)
+# print(val)
 while True:
-    # print(state)
+    print(state)
     if player == 1:
         valid_moves = ttt.get_valid_moves(state)
-        print("valid_moves", [i for i in range(ttt.action_size) if valid_moves[i] == 1])
         action = int(input("Enter action: "))
-        if valid_moves[action] == 0:
-            print("action not valid")
-            continue
 
     else:
         neutral_state = state
-        print(neutral_state, "neutral_state")
         mcts_probs = mcts.search(neutral_state)
         action = np.argmax(mcts_probs)
-
     state = ttt.get_next_state(state, action, player)
     value, is_terminal = ttt.get_value_and_terminated(state, action)
-
-    # if is_terminal:
-    #     print(state)
-    #     if value == 1:
-    #         print("Player", player, "wins")
-    #     else:
-    #         print("Draw")
-    #     break
+    if is_terminal:
+        print(state)
+        if value == 1:
+            print("Player", player, "wins")
+        else:
+            print("Draw")
+        break
 
     player = ttt.get_opponent(player)
-    print(player)
-    print(state)
