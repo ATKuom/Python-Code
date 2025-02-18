9f40e70041de93662c88272a2929dbce500663af
Ali Tarik Karagoz <131875679+ATKuom@users.noreply.github.com>
2025-02-18 16:33:14 +0000

    18/02 MCTS,alphazero trials

--- mcts.py
+++ mcts.py
@@ -8,6 +8,7 @@ from ZW_model import GPT
 from ZW_Opt import *
 from split_functions import bound_creation, layout_to_string_single_1d
 from thermo_validity import *
+from tqdm.notebook import trange
 
 model = GPT(12, 32, 4, 2, 22, 0.1)
 model.load_state_dict(torch.load("GPT_NA_psitest/M1_model_10.pt"))
@@ -22,8 +23,8 @@ def evaluation(layout):
     ]
     valid_string = validity(stringlist)
     if len(valid_string) == 0:
-        return -1e6
-    ohe = np.zeros((len(layout), len(classes)))
+        return -100
+    ohe = np.zeros((len(layout), len(classes)), dtype=object)
     for i, l in enumerate(layout):
         ohe[i, l] = 1
     equipment, bounds, x, splitter = bound_creation(ohe)
@@ -37,9 +38,13 @@ def evaluation(layout):
     iterations = 30
     try:
         a = PSO(objective_function, bounds, particle_size, iterations, nv, equipment)
-        value = -1 * a.result
+        if a.result < 1e6:
+            value = a.result
+            print(valid_string, value)
+        else:
+            value = -5
     except:
-        value = -1e6
+        value = -10
     return value
 
 
@@ -57,7 +62,10 @@ class Flowsheet:
         return blank_state
 
     def get_next_state(self, state, action, player=1):
-        column = np.where(state == -1)[0][0]
+        try:
+            column = np.where(state == -1)[0][0]
+        except:
+            column = self.column_count
         state[column] = action
         return state
 
@@ -80,7 +88,7 @@ class Flowsheet:
         return 0, False
 
     def get_opponent(self, player):
-        return -player
+        return player
 
     def get_opponent_value(self, value):
         return value
@@ -89,7 +97,10 @@ class Flowsheet:
         return state
 
     def get_encoded_state(self, state):
-        column = np.where(state == -1)[0][0]
+        try:
+            column = np.where(state == -1)[0][0]
+        except:
+            column = self.column_count
         encoded_state = state[:column]
         return encoded_state
 
@@ -141,7 +152,6 @@ class Node:
             if prob > 0:
                 child_state = self.state.copy()
                 child_state = self.game.get_next_state(child_state, action, 1)
-                child_state = self.game.change_perspective(child_state, player=-1)
 
                 child = Node(self.game, self.args, child_state, self, action, prob)
                 self.children.append(child)
@@ -171,13 +181,14 @@ class MCTS:
         )[:, -1, :]
         policy = torch.softmax(policy, axis=-1).squeeze(0).detach().numpy()
 
-        # policy = (1 - self.args["dirichlet_epsilon"]) * policy + self.args[
-        #     "dirichlet_epsilon"
-        # ] * np.random.dirichlet([self.args["dirichlet_alpha"]] * self.game.action_size)
-
+        policy = (1 - self.args["dirichlet_epsilon"]) * policy + self.args[
+            "dirichlet_epsilon"
+        ] * np.random.dirichlet([self.args["dirichlet_alpha"]] * self.game.action_size)
         # all moves are valid if we are not masking valid_moves = self.game.get_valid_moves(state)
-        # policy*=valid_moves
-        # policy /= np.sum(policy)
+        valid_moves = np.ones(self.game.action_size)
+        valid_moves[0], valid_moves[6], valid_moves[8], valid_moves[10] = 0, 0, 0, 0
+        policy *= valid_moves
+        policy /= np.sum(policy)
         root.expand(policy)
         for search in range(self.args["num_searches"]):
             # selection
@@ -199,10 +210,15 @@ class MCTS:
                     ).unsqueeze(0)
                 )[:, -1, :]
                 policy = torch.softmax(policy, axis=-1).squeeze(0).detach().numpy()
-                # valid_moves = self.game.get_valid_moves(node.state)
-                # policy = policy * valid_moves
-                # policy /= np.sum(policy)
-
+                valid_moves = np.ones(self.game.action_size)
+                valid_moves[0], valid_moves[6], valid_moves[8], valid_moves[10] = (
+                    0,
+                    0,
+                    0,
+                    0,
+                )
+                policy = policy * valid_moves
+                policy /= np.sum(policy)
                 # value = value.item()
                 value = 0.5
                 # expansion
@@ -218,38 +234,117 @@ class MCTS:
         # return visit_counts
 
 
-ttt = Flowsheet()
-player = 1
+class Alphazero:
+    def __init__(self, model, optimizer, game, args):
+        self.model = model
+        self.optimizer = optimizer
+        self.game = game
+        self.args = args
+        self.mcts = MCTS(game, args, model)
+
+    def selfPlay(self):
+        memory = []
+        player = 1
+        state = self.game.get_initial_state()
+
+        while True:
+            action_probs = self.mcts.search(state)
+            memory.append((state, action_probs, player))
+            # Temperature lim 0 exploiation, lim inf exploration (more randomness)
+            temperature_action_probs = action_probs ** (1 / self.args["temperature"])
+            temperature_action_probs /= np.sum(temperature_action_probs)
+
+            action = np.random.choice(self.game.action_size, p=temperature_action_probs)
+            state = self.game.get_next_state(state, action, player)
+            value, is_terminal = self.game.get_value_and_terminated(state, action)
+            if is_terminal:
+                returnMemory = []
+                for hist_neutral_state, hist_action_probs, hist_player in memory:
+                    hist_outcome = (
+                        value
+                        if hist_player == player
+                        else self.game.get_opponent_value(value)
+                    )
+                    returnMemory.append(
+                        (
+                            self.game.get_encoded_state(hist_neutral_state),
+                            hist_action_probs,
+                            hist_outcome,
+                        )
+                    )
+                return returnMemory
+            player = self.game.get_opponent(player)
+
+    def train(self, memory):
+        random.shuffle(memory)
+        for batchIdx in range(0, len(memory), self.args["batch_size"]):
+            sample = memory[
+                batchIdx : min(len(memory) - 1, batchIdx + self.args["batch_size"])
+            ]
+            state, policy_targets, value_targets = zip(*sample)
+            state, policy_targets, value_targets = (
+                np.array(state),
+                np.array(policy_targets),
+                np.array(value_targets).reshape(-1, 1),
+            )
+            state = torch.tensor(state).float()
+            policy_targets = torch.tensor(policy_targets).float()
+            value_targets = torch.tensor(value_targets).float()
+
+            out_policy, out_value = self.model(state)
+
+            policy_loss = F.cross_entropy(out_policy, policy_targets)
+            value_loss = F.mse_loss(out_value, value_targets)
+            loss = policy_loss + value_loss
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+    def learn(self):
+        for iteration in range(self.args["num_iterations"]):
+            memory = []
+
+            for selfPlay_iteration in trange(self.args["num_selfPlay_iterations"]):
+                memory += self.selfPlay()
+            self.model.train()
+            for epoch in trange(self.args["num_epochs"]):
+                self.train(memory)
+
+            save_path = self.args["save_path"]
+            torch.save(
+                self.model.state_dict(), f"{save_path}/model_{iteration}_{self.game}.pt"
+            )
+            torch.save(
+                self.optimizer.state_dict(),
+                f"{save_path}/optimizer_{iteration}_{self.game}.pt",
+            )
+
+
+game = Flowsheet()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-4)
+# args = {
+#     "C":2,
+#     "num_searches":60,
+#     "num_iterations":3,
+#     "num_selfPlay_iterations":500,
+#     "num_epochs":4,
+#     "batch_size":64,
+#     "temperature":1.25,
+#     "dirichlet_epsilon":0.25,
+#     "dirichlet_alpha":0.3
+# }
 args = {
-    "C": 1.41,
-    "num_searches": 10,
+    "C": 2,
+    "num_searches": 500,
+    "num_iterations": 4,
+    "num_selfPlay_iterations": 100,
+    "num_epochs": 4,
+    "batch_size": 128,
+    "temperature": 1.25,
+    "dirichlet_epsilon": 0.25,
+    "dirichlet_alpha": 0.3,
+    "save_path": "./RL/policy_model_trials",
 }
-mcts = MCTS(ttt, args, model)
-state = ttt.get_initial_state()
-# state = [0, 1, 5, 2, 3, 9, 4, 7, 5, 4, 7, 4, 11, -1]
-# state = np.array(state)
-# sf = ttt.get_encoded_state(state)
-# print(sf)
-# val = evaluation(sf)
-# print(val)
-while True:
-    print(state)
-    if player == 1:
-        valid_moves = ttt.get_valid_moves(state)
-        action = int(input("Enter action: "))
-
-    else:
-        neutral_state = state
-        mcts_probs = mcts.search(neutral_state)
-        action = np.argmax(mcts_probs)
-    state = ttt.get_next_state(state, action, player)
-    value, is_terminal = ttt.get_value_and_terminated(state, action)
-    if is_terminal:
-        print(state)
-        if value == 1:
-            print("Player", player, "wins")
-        else:
-            print("Draw")
-        break
-
-    player = ttt.get_opponent(player)
+alphazero = Alphazero(model, optimizer, game, args)
+alphazero.learn()
